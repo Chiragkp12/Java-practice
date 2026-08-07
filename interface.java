81a1d2d6e445a2f9a7dcf619e7e36007f0fbd6dc
Chirag K P <147081185+Chiragkp12@users.noreply.github.com>
2026-08-07 11:53:58 +0530

    Update interface.java

--- interface.java
+++ interface.java
@@ -106,5 +106,13 @@ Rule of interface
 1. an Interface is like a contract, like when implemented,helps to achieve- standardization
 2. Interface promote polymorphism by allowing an interface type refernce to point to object of implementing classes.its achieves loose coupling, rduces code and provide code flexibility
 3. Methods within in an interface are automatically public and abstract.
-4. 
+4. specialised methods cannot be accessed directly using interface type reference
+5. if a class partially implements interface it must declare itself as abstract
+6. A class can have multiple interface because diamond shape problem does not exist as interface does not have a parent
+7. An interface cannot implement another interface, because interface cannot provide body
+8. An interface can inherit from another interface.Not only this it can inherit from multiple interfaces because daimond shape problem does not exist multiple inheritance in java can be indirectly achieved by using multiple interface 
+9. A class can both extend another class as well as implement instance.However it extends first and then implemnets instance then extends
+10. An interface can conatin costant variable and method signatures. A variable within an interface ia automatically public static final
+11. An empty interface in java is referred toa s marked interface or tagged interface. it is used to provide special properties to the object of the class/
+12. An object of an interface cannot be created because an interface is a collection of abstract methods. however reference can be created so that loose coupling, polymorphism and its advantages can be achieved
 */
